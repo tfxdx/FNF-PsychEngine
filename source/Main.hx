f2800c5cca4129b54e0fe0a7434519d2ffdbb890
tfxdx <yunumendozaperez@gmail.com>
2026-04-14 15:40:28 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -190,6 +190,8 @@ class Main extends Sprite
 
 		// shader coords fix
 		FlxG.signals.gameResized.add(function (w, h) {
+			if(fpsVar != null)
+				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
 		     if (FlxG.cameras != null) {
 			   for (cam in FlxG.cameras.list) {
 				if (cam != null && cam.filters != null)
